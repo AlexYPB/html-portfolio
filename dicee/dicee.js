2f1d53fdec8b43c46eb03a445ccbd53781cbae2d
AlexYPB <alex.pizarroburgos@gmail.com>
2026-09-18 17:53:06 -0400

    Fix file and image linking and make files lowercase

--- dicee/dicee.js
+++ dicee/dicee.js
@@ -1,5 +1,5 @@
 var randomNumber1 = Math.floor((Math.random() * 6)+1);
-document.querySelector(".img1").src = "images/dice" + randomNumber1 + ".png";
+document.querySelector(".img1").src = "./images/dice" + randomNumber1 + ".png";
 
 var randomNumber2 = Math.floor((Math.random() * 6)+1);
 document.querySelector(".img2").src = "images/dice" + randomNumber2 + ".png";
